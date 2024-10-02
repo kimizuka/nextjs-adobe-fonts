87e0a68d8cdfad6e283885274757f1b8a5d08ad0
kimizuka <f.kimizuka@gmail.com>
2024-10-02 10:16:49 +0900

    add client components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,27 +13,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(d) {
-              var config = {
-                kitId: '${process.env.NEXT_PUBLIC_KIT_ID}',
-                scriptTimeout: 3000,
-                async: true,
-                active: () => {
-                  document.fonts.ready.then(() => {
-                    window.typekitLoaded = true;
-                  });
-                },
-              },
-              h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-            })(document);
-          `,
-          }}
-        />
-      </head>
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import styles from "./index.module.css";
+import { AdobeFonts } from '@/components/AdobeFonts';
 
 export default function FontPage() {
   return (
     <div>
+      <AdobeFonts />
       <p className={ styles["toppan-bunkyu-midashi-go-std"] }>あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。</p>
     </div>
   );

b7e5f94dd6d40217a4ee143ff45816a4b2486683
kimizuka <f.kimizuka@gmail.com>
2024-10-02 10:05:23 +0900

    :)

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.css";
+
+export default function FontPage() {
+  return (
+    <div>
+      <p className={ styles["toppan-bunkyu-midashi-go-std"] }>あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。</p>
+    </div>
+  );
+}
